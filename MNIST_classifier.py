ab71d6e4fe4a343b622fd0abd6eb696acf1e2af9
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-01-22 23:25:54 -0500

    post-test working
    Final commit, working model.

--- MNIST_classifier.py
+++ MNIST_classifier.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
@@ -69,11 +68,11 @@ with gph.as_default():
 
     initializer = tf.contrib.layers.xavier_initializer()
 
-    conv1 = tf.layers.conv2d(x,32,[5,5],[1,1],padding= 'same',activation = tf.nn.relu(),use_bias=True,kernel_initializer=initializer,
+    conv1 = tf.layers.conv2d(x,32,[5,5],[1,1],padding= 'same',activation = tf.nn.relu,use_bias=True,kernel_initializer=initializer,
                              bias_initializer=tf.zeros_initializer(),trainable=True,name = "conv1")
     pool1 = tf.layers.max_pooling2d(conv1,[2,2],strides = [2,2],padding= "same",name = "pool1")
 
-    conv2 = tf.layers.conv2d(pool1,64,[5,5],[1,1],padding = "same",activation=tf.nn.relu(),
+    conv2 = tf.layers.conv2d(pool1,64,[5,5],[1,1],padding = "same",activation=tf.nn.relu,
                              use_bias=True,kernel_initializer=initializer,
                              bias_initializer=tf.zeros_initializer(),trainable=True,name = "conv2")
     pool2 = tf.layers.max_pooling2d(conv2, [2, 2], strides=[2, 2], padding="same", name="pool2")
@@ -81,7 +80,7 @@ with gph.as_default():
     flat_tensor = tf.layers.flatten(pool2,name = "flat_tensor")
 
     fc1 = tf.layers.dense(flat_tensor,units = 1024,
-                          activation = tf.nn.relu(),
+                          activation = tf.nn.relu,
                           use_bias=True,
                           kernel_initializer=initializer,
                           bias_initializer=tf.zeros_initializer(),
@@ -96,41 +95,12 @@ with gph.as_default():
                           trainable=True,
                           name = "fc2")
 
-    '''
-    kern = [0]*2
-    bias = [0]*2
-    ls_filt_dims = [[5,5,1,32],[5,5,32,64]]
-    fmap = x
-    for i in range(2):
-        kern[i] = tf.Variable(initializer(shape = ls_filt_dims[i]))
-        bias[i] = tf.Variable(initializer(shape = [ls_filt_dims[i][3]]))
-        fmap = tf.nn.conv2d(fmap,kern[i],[1,1,1,1],'SAME')
-        layer = tf.nn.relu(fmap + bias[i])
-        layer_pool = tf.nn.max_pool(layer,[1,2,2,1],[1,2,2,1],'SAME')
-        fmap = layer_pool
-
-    # the list out_dims is calculated manually
-    out_dims = [tf.shape(x)[0],7,7,64]
-    flat_dim = out_dims[1]*out_dims[2]*out_dims[3]
-
-    # fully connected layer
-    fw = [0]*2
-    fb = [0]*2
-
-    ls_weight_dims = [[flat_dim, 1024], [1024, 10]]
-    fa = tf.reshape(fmap,shape = (out_dims[0],flat_dim))
-    for i in range(2):
-        fw[i] = tf.Variable(initializer(shape = ls_weight_dims[i]))
-        fb[i] = tf.Variable(initializer(shape = [ls_weight_dims[i][1]]))
-        fz = tf.matmul(fa,fw[i])+fb[i]
-        fa = tf.nn.relu(fz)
-'''
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = fc2,labels=y_true))
 
     # calculating accuracy
     y_pred = tf.nn.softmax(logits = fc2)
     y_pred_cls = tf.argmax(y_pred,axis =1)
-    _,acc = tf.metrics.accuracy(tf.argmax(y_pred_cls,y_true_cls))
+    _,acc = tf.metrics.accuracy(y_pred_cls,y_true_cls)
 
     global_step = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(start_learning,global_step,100,0.96,staircase=True)
@@ -145,22 +115,9 @@ with tf.Session(graph=gph) as sess:
     train_data = Data()
     train_data.get_xdata("data/x_train.csv")
     train_data.get_ydata("data/y_train.csv")
-
-    i = 0
-    count = 0
-    while(i<epochs):
+    for i in range(epochs):
         i = i+1
-        count += 1
         train_data.get_rand_batch(batch_size)
-        _,train_acc = sess.run([opt,acc],feed_dict={x:train_data.x_batch,y:train_data.y_batch})
+        feed_dict = {x:train_data.x_batch, y_true: train_data.y_batch}
+        _,train_acc = sess.run([opt,acc],feed_dict)
         print("epoch: ",i,"\tAccuracy: ",train_acc)
-
-        if i == epochs-1:
-            flag = input("Continue(y/n): ")
-            if flag == 'y':
-                i = 0
-            else:
-                break
-
-    # saver.save(sess,"cnnmodel/model1")
-    plt.show()
