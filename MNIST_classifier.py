b0cf2e7bfe3e7d3d95d0fa361252ff44cef7255e
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-03-13 02:46:12 -0400

    project dumpster commit 1
    Task 2 and 3 complete

--- MNIST_classifier.py
+++ MNIST_classifier.py
@@ -62,40 +62,45 @@ gph = tf.Graph()
 with gph.as_default():
 
     # define placeholders
-    x = tf.placeholder('float',shape = (None,28,28,1))
-    y_true = tf.placeholder('float',shape = (None,10))
+    with tf.variable_scope("inputs"):
+        x = tf.placeholder('float',shape = (None,28,28,1),name ="input_img")
+        y_true = tf.placeholder('float',shape = (None,10),name = "true_cls")
+
     y_true_cls = tf.argmax(y_true,axis = 1)
 
     initializer = tf.contrib.layers.xavier_initializer()
 
-    conv1 = tf.layers.conv2d(x,32,[5,5],[1,1],padding= 'same',activation = tf.nn.relu,use_bias=True,kernel_initializer=initializer,
+    with tf.variable_scope("layer1"):
+        conv1 = tf.layers.conv2d(x,32,[5,5],[1,1],padding= 'same',activation = tf.nn.relu,use_bias=True,kernel_initializer=initializer,
                              bias_initializer=tf.zeros_initializer(),trainable=True,name = "conv1")
-    pool1 = tf.layers.max_pooling2d(conv1,[2,2],strides = [2,2],padding= "same",name = "pool1")
+        pool1 = tf.layers.max_pooling2d(conv1,[2,2],strides = [2,2],padding= "same",name = "pool1")
 
-    conv2 = tf.layers.conv2d(pool1,64,[5,5],[1,1],padding = "same",activation=tf.nn.relu,
+    with tf.variable_scope("layer2"):
+        conv2 = tf.layers.conv2d(pool1,64,[5,5],[1,1],padding = "same",activation=tf.nn.relu,
                              use_bias=True,kernel_initializer=initializer,
                              bias_initializer=tf.zeros_initializer(),trainable=True,name = "conv2")
-    pool2 = tf.layers.max_pooling2d(conv2, [2, 2], strides=[2, 2], padding="same", name="pool2")
+        pool2 = tf.layers.max_pooling2d(conv2, [2, 2], strides=[2, 2], padding="same", name="pool2")
 
     flat_tensor = tf.layers.flatten(pool2,name = "flat_tensor")
 
-    fc1 = tf.layers.dense(flat_tensor,units = 1024,
-                          activation = tf.nn.relu,
-                          use_bias=True,
-                          kernel_initializer=initializer,
-                          bias_initializer=tf.zeros_initializer(),
-                          trainable=True,
-                          name = "fc1")
+    with tf.variable_scope("fully_connect_layers"):
+        fc1 = tf.layers.dense(flat_tensor,units = 1024,
+                              activation = tf.nn.relu,
+                              use_bias=True,
+                              kernel_initializer=initializer,
+                              bias_initializer=tf.zeros_initializer(),
+                              trainable=True,
+                              name = "fc1")
 
-    fc2 = tf.layers.dense(fc1,units = 10,
-                          activation = None,
-                          use_bias = True,
-                          kernel_initializer=initializer,
-                          bias_initializer=tf.zeros_initializer(),
-                          trainable=True,
-                          name = "fc2")
+        fc2 = tf.layers.dense(fc1,units = 10,
+                              activation = None,
+                              use_bias = True,
+                              kernel_initializer=initializer,
+                              bias_initializer=tf.zeros_initializer(),
+                              trainable=True,
+                              name = "fc2")
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = fc2,labels=y_true))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = fc2,labels=y_true),name = "cost")
 
     # calculating accuracy
     y_pred = tf.nn.softmax(logits = fc2)
@@ -106,8 +111,7 @@ with gph.as_default():
     learning_rate = tf.train.exponential_decay(start_learning,global_step,100,0.96,staircase=True)
     # updates learning rate for every 100 epochs since we are doing batch training
 
-    opt = tf.train.AdamOptimizer(learning_rate).minimize(cost,global_step=global_step)
-    saver = tf.train.Saver()
+    opt = tf.train.AdamOptimizer(learning_rate).minimize(cost,global_step=global_step,name = "optimizer")
 
 with tf.Session(graph=gph) as sess:
     sess.run(tf.global_variables_initializer())
